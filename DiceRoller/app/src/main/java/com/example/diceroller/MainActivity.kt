f298e3943e7c8c21e29638dea0a7af1c2927b7c9
Rusnak Volodymyr <vrusna@softserveinc.com>
2023-03-27 20:27:03 +0300

    add logic to dice roll app

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,13 +2,30 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private fun rollDice() {
+        val dice = Dice(6)
+        val resultTextView: TextView = findViewById(R.id.textView)
+        val resultTextView2: TextView = findViewById(R.id.textView2)
+        resultTextView.text = dice.roll().toString()
+        resultTextView2.text = dice.roll().toString()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener {
+            rollDice();
+        }
+    }
+}
 
+class Dice(val numSides: Int){
+    fun roll(): Int {
+        return (1..numSides).random();
     }
 }
